fb2d8e2a6f97249613733791f148ff3df1ee7923
lautarograciani <lautaro@graciani.ar>
2023-04-05 02:57:14 -0300

    added some extra cfg

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Entry point for the build script in your package.json.
-import "@hotwired/turbo-rails"
+import "@hotwired/turbo-rails";
 // Stimulus controllers.
-import "./controllers"
-import "./components/counter">>>>>>> b22e4da6e30f951d7734c2175234b7958d7aa20b
+import "./controllers";
+import "./components/counter";
