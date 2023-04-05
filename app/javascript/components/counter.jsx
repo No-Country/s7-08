4b423d73eed7ceb495456d64781b2f7cb2117f0c
lautarograciani <lautaro@graciani.ar>
2023-04-05 02:29:06 -0300

    new config

---
+++ app/javascript/components/counter.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { createRoot } from 'react-dom/client';
+
+const Counter = ({ arg }) => {
+  const [count, setCount] = useState(0);
+  const countRef = useRef(count);
+  countRef.current = count;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(countRef.current + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return <div>{`${arg} - counter = ${count}!`}</div>;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("root");
+  const root = createRoot(container);
+  root.render(<Counter arg={`
+    Ruby ${container.getAttribute('ruby')}
+    Rails ${container.getAttribute('rails')}`} />);
+});
