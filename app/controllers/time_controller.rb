06c5526b2b4eba4010ad68901f0bf2906888ad60
lautarograciani <lautaro@graciani.ar>
2023-04-16 07:21:52 -0300

    commit

--- app/controllers/time_controller.rb
+++
@@ -1,5 +0,0 @@
-class TimeController < ApplicationController
-  def index
-    #código placeholder para probar que javascript esbuild fuinciona
-  end
-end
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: [
-    "./app/**/*.html.erb",
-    "./app/helpers/**/*.rb",
-    "./app/javascript/**/*.js",
-  ],
-  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
-};
