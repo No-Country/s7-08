a17cf4c1ef25a1a7bbe89e3a7a7a14af2dc23457
rodrigo ladron de guevara <ro.ladrondeguevara@gmail.com>
2023-04-24 10:37:15 -0300

    release -1

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,38 @@ admin = User.create(email: 'admin@gmail.com',
                    password: 'password',
                    password_confirmation: 'password',
                    role: 'admin',
-                   username: 'Admin')
+                   username: 'Rodrigo')
 User.create(email: 'user@gmail.com',
             password: 'password',
             password_confirmation: 'password',
-            username: 'User')
+            username: 'Franco')
 User.create(email: 'user2@gmail.com',
             password: 'password',
             password_confirmation: 'password',
-            username: 'User2')
+            username: 'Milton')
+User.create(email: 'user3@gmail.com',
+            password: 'password',
+            password_confirmation: 'password',
+            username: 'Leandro')
+User.create(email: 'user4@gmail.com',
+            password: 'password',
+            password_confirmation: 'password',
+            username: 'Paulo')   
+User.create(email: 'user5@gmail.com',
+            password: 'password',
+            password_confirmation: 'password',
+            username: 'Enzo')        
+User.create(email: 'user6@gmail.com',
+            password: 'password',
+            password_confirmation: 'password',
+            username: 'Nacho')  
+User.create(email: 'user7@gmail.com',
+            password: 'password',
+            password_confirmation: 'password',
+            username: 'Rodri')    
+User.create(email: 'user8@gmail.com',
+            password: 'password',
+            password_confirmation: 'password',
+            username: 'Lucas')                                               
 admin.joined_rooms << Room.create(name: 'General', is_private: false)
 admin.joined_rooms << Room.create(name: 'Testing', is_private: false)
